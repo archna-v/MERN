af1251fcd87d422637a03e8e732d381b670568d6
iamarchna <archna.coding@gmail.com>
2024-12-28 11:51:01 +0530

    day 2 topics

---
+++ operators.js
@@ -0,0 +1,30 @@
+//unary opertor 
+a=10
+a++;
+console.log(a);
+++a;
+
+//bitwise opertor
+s=5
+d=1
+console.log(s&d);
+
+//relational opertor 
+var rel1=30
+var rel2="30"
+if(rel1==rel2)
+{
+    console.log("both are not same type");
+}
+
+//logical opertor
+x=90
+y=80
+if(x>10&&y>10)
+    console.log("not same");
+if(!x==90)
+    console.log("x is not 90")
+
+//ternary opertor 
+console.log(x>y? "x is greater" : "y is greater");
+
