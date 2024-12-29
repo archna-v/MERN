6a449f45d944a3f80826312ac09da9d6181c09fb
iamarchna <archna.coding@gmail.com>
2024-12-29 14:12:24 +0530

    Rename folder 'day 1' to 'day_1'

--- function.js
+++
@@ -1,7 +0,0 @@
-//Arrow function 
-arrowFun = (studentDept) => {
-            return studentDept[1];
-
-}
-console.log(arrowFun(['AIML,CSE,IT',"AIML","CSE","IT"]));
-//destructuring opertaor 
--- objects.js
+++
@@ -1,5 +0,0 @@
-ob={}
-ob["movie"]="Bahubali"
-ob["budget"]="100 cr"
-ob["release"]="worldwide"
-console.log(ob);
